8d367c19de64f2b61f548fe226e4ad97246a9daa
fabionovais <30811155+fabionovais@users.noreply.github.com>
2018-10-25 14:48:10 -0300

    Update MySQL Server Settings

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','osticketsrv10.mysql.database.azure.com');
+define('DBHOST','osticketsrvprod1.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@osticketsrv10');
+define('DBUSER','demouser@osticketsrvprod1');
 define('DBPASS','demo@pass123');
 
 # Table prefix
